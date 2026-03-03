1342aef47703cfb361aec214e85e3611670374d6
lokeshtewatia-02 <lokeshtewatia003@gmail.com>
2026-03-03 22:36:09 +0530

    Dynamic.java

--- Dynamic.java
+++ Dynamic.java
@@ -12,7 +12,7 @@ Class.forName("com.mysql.jdbc.Driver");
 //create a connection
 String url="jdbc:mysql://localhost:3306/youtube";
 String usename="root";
-String password="2003@lokesh";
+String password="Enter your password:";
 
 Connection con=DriverManager.getConnection(url,usename,password);
 
